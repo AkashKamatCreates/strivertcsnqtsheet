45e7a54f60b66f08ce96460731e7d6d2c7c800a9
akash <kamatakash13@gmail.com>
2024-01-13 14:15:51 +0530

    String16 file added through terminal

---
+++ src/main/java/org/akashkamat/string16.java
@@ -0,0 +1,19 @@
+package org.akashkamat;
+
+public class string16 {
+    public static void main(String[] args) {
+        String str = "takeuforward";
+        System.out.println("Maximum occurring character is " + maxOccurringChar(str));
+    }
+
+    static char maxOccurringChar(String s){
+        int[] frequency = new int[256];
+        for(int i=0; i<s.length(); i++){
+            frequency[s.charAt(i)]++;
+        }
+        continue contineu
+
+
+        return (char)(max);
+    }
+}
