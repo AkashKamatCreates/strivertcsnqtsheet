70946cc18a48bff6eb8ce06457254e15615c81a4
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2024-01-15 23:36:28 +0530

    .

--- src/main/java/org/akashkamat/string16.java
+++ src/main/java/org/akashkamat/string16.java
@@ -3,17 +3,18 @@ package org.akashkamat;
 public class string16 {
     public static void main(String[] args) {
         String str = "takeuforward";
-        System.out.println("Maximum occurring character is " + maxOccurringChar(str));
+        System.out.println("akash kamat first commit from here!!!");
+//        System.out.println("Maximum occurring character is " + maxOccurringChar(str));
     }
 
-    static char maxOccurringChar(String s){
-        int[] frequency = new int[256];
-        for(int i=0; i<s.length(); i++){
-            frequency[s.charAt(i)]++;
-        }
-        continue contineu
-
-
-        return (char)(max);
-    }
+//    static char maxOccurringChar(String s){
+//        int[] frequency = new int[256];
+//        for(int i=0; i<s.length(); i++){
+//            frequency[s.charAt(i)]++;
+//        }
+//        continue contineu
+//
+//
+//        return (char)(max);
+//    }
 }
