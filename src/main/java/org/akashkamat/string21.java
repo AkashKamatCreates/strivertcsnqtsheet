a48509c82d0f763cd94443121bbe29c4114a6ad8
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2024-01-17 10:43:40 +0530

    commit

--- src/main/java/org/akashkamat/string21.java
+++ src/main/java/org/akashkamat/string21.java
@@ -2,19 +2,61 @@ package org.akashkamat;
 
 public class string21 {
     public static void main(String[] args) {
-        String str = "Google Docs";
+        String str = "Google Docs anotherlongword";
         System.out.print("Largest Word is: "+ LargestWord(str));
     }
 
-    static int LargestWord(String s){
-        int count = 0;
-        int max = 0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i+1)!=' ' && i!=s.length()-1){
-                count++;
-                max = Math.max(max, count);
+    static String LargestWord(String s){
+        int i=0;
+        int j=0;
+        int len = s.length();
+        int maxlen = 0;
+        int maxstart = 0;
+        StringBuilder result = new StringBuilder();
+
+        while(j<=len){
+            if(j<len && s.charAt(j)!=' '){
+                j++;
+            }else{
+                int currentlength = j-i;
+                if(currentlength>maxlen){
+                    maxlen = currentlength;
+                    maxstart = i;
+                }
+                j++;
+                i=j;
             }
         }
-        return max;
+        result = result.append(s.substring(maxstart, maxstart+maxlen));
+        return result.toString();
     }
+
+
+
+
+
+//    static String LargestWord(String s){
+//        int i=0;
+//        int j=0;
+//        int len = s.length();
+//        StringBuilder result = new StringBuilder();
+//        int maxlen = 0;
+//        int maxstart = 0;
+//        while(j<=len){
+//            if(j<len && s.charAt(j)!=' '){
+//                j++;
+//            }else{
+//                int currentlength = j-i;
+//                if(currentlength > maxlen){
+//                    maxlen = currentlength;
+//                    maxstart = i;
+//                }
+//                j++;
+//                i=j;
+//            }
+//        }
+//        result = result.append(s.substring(maxstart, maxstart+maxlen));
+//        return result.toString();
+//
+//    }
 }
